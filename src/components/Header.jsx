ffc94d0b4b255b4ce205b8d6fc5576f7b282198f
Sanjeeban Das <iamsanjeebandas@gmail.com>
2024-07-19 00:09:46 +0530

    added dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from "next/link.js";
 import MenuItem from "./MenuItem.jsx";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import DarkModeSwitch from "../components/DarkModeSwitch.jsx";
 export default function Header() {
   return (
     <div className="flex justify-between items-center p-3 max-w-6xl mx-auto">
@@ -9,12 +10,15 @@ export default function Header() {
         <MenuItem title="home" address="/" Icon={AiFillHome} />
         <MenuItem title="about" address="/about" Icon={BsFillInfoCircleFill} />
       </div>
-      <Link href={"/"} className="flex gap-1 items-center">
-        <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
-          Film
-        </span>
-        <span className="text-xl hidden sm:inline">Flock</span>
-      </Link>
+      <div className="flex items-center gap-4">
+        <DarkModeSwitch />
+        <Link href={"/"} className="flex gap-1 items-center">
+          <span className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg">
+            Film
+          </span>
+          <span className="text-xl hidden sm:inline">Flock</span>
+        </Link>
+      </div>
     </div>
   );
 }
