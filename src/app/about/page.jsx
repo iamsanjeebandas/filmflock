30ceb59a80437598746ca9ef3ac06c5fd8f527e2
Sanjeeban Das <iamsanjeebandas@gmail.com>
2024-07-19 20:01:52 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,33 @@
+export default function page() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to Film Flock, your ultimate destination for everything related
+        to movies and TV shows! Our platform is designed to bring together movie
+        enthusiasts from all over the world, creating a comprehensive hub where
+        you can discover, explore, and dive deep into the world of cinema and
+        television. Whether you're looking for the latest blockbusters, timeless
+        classics, or hidden gems, Film Flock is your go-to source for
+        discovering new and exciting content.
+      </p>
+      <p>
+        At Film Flock, we are passionate about movies and TV shows. Our mission
+        is to provide a user-friendly platform where you can search for any
+        movie, view detailed information, and see ratings and reviews. Get
+        comprehensive details about your favorite movies and TV shows, including
+        cast and crew information, plot summaries, and user ratings. Our
+        extensive database covers a vast range of movies and TV shows from all
+        genres and eras, ensuring you always have something new to explore.
+      </p>
+      <p>
+        Join the Film Flock community and be a part of our growing family.
+        Whether you're a casual viewer or a hardcore cinephile, Film Flock has
+        something for everyone. Sign up today and start exploring the wonderful
+        world of movies and TV shows with us. Thank you for visiting Film Flock.
+        We hope you enjoy your time here and find our platform to be a valuable
+        resource for all your entertainment needs.
+      </p>
+    </div>
+  );
+}
